f99d99002298ff63b556f273f2606a126d13397c
Murodbek <gulmatovmurodbek5@gmail.com>
2025-03-01 06:38:56 +0300

    Portfolio

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+(function () {
+    emailjs.init("7L36WTC-cbuJJaVDq");
+})();
+document.getElementById('submit').addEventListener('click', function (event) {
+    event.preventDefault();
+    let message = document.getElementById('message').value,
+        email = document.getElementById('email').value,
+        name = document.getElementById('name').value;
+
+    console.log(name, email, message);
+
+    emailjs.send("service_jiprdt", "template_uvfhdfe", {
+        to_name: "Murodbek",
+        message: message,
+        email_id: email,
+        from_name: name,
+    }).then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+    }, function (error) {
+        console.log('FAILED...', error);
+    });
+});
